7f89c206bec8acab62368a9e04d37795de498374
johnhof <john.hofrichter@gmail.com>
2015-10-16 11:23:09 -0700

    fixed auth bug & added styling standards

--- examples/express.js
+++ examples/express.js
@@ -2,16 +2,17 @@ var express      = require('express');
 var swagger      = require('swagger-injector');
 var cookieParser = require('cookie-parser');
 var app          = express();
+var port = process.env.NODE_ENV || 5000;
 
 app.use(cookieParser());
 app.use(swagger.express({
-  restrict : {
-    key : {
-      name  : 'swag',
-      value : 'swagbag'
+  restrict: {
+    key: {
+      name: 'swag',
+      value: 'swagbag'
     }
   },
-  unauthorized : function (req, res) {
+  unauthorized: function (req, res) {
     res.status(403).send('Forbidden');
   }
 }));
@@ -20,4 +21,5 @@ app.use(function (req, res, next) {
   res.send('OK');
 });
 
-app.listen(5000);
+app.listen(port);
+console.log('Listening on port ' + port);
